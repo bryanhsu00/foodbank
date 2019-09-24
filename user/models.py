25ad23512355195763a86bc9f8cf6a1c5c9ad3db
AT-J <s104213080@mail1.ncnu.edu.tw>
2019-09-24 21:18:28 +0800

    user_api

--- user/models.py
+++ user/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class User(AbstractUser):
     id = models.AutoField(primary_key=True,verbose_name="編號")
     phone_number = models.CharField(null=True,blank=True,max_length=20,verbose_name="電話號碼")
--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse,HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.conf import settings
+from .models import *
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                auth.login(request,user)
+                return HttpResponse("登入成功")
+    return HttpResponse("登入失敗")
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponse("已登出") 
+
+def sign_up(request):
+    username = request.POST.get("username")
+    tmp = User.objects.filter(username=username)
+    if len(tmp) > 0:
+        return HttpResponse("此帳號已經被使用")
+    password = request.POST.get("password")
+    password_check = request.POST.get("password_check")
+    if password != password_check:
+        return HttpResponse("請確認您的密碼")
+
+    new_user = User.objects.create_user(username=username,password=password)
+    # new_user.is_active = 1 #若無需驗證
+    new_user.save()
+
+    return HttpResponse("註冊成功")
