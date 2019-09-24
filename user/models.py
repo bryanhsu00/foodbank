338e6841d6798ecde4770fbe23dac767d5419bf0
AT-J <s104213080@mail1.ncnu.edu.tw>
2019-09-24 18:23:12 +0800

    user_model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True,verbose_name="編號")
+    phone_number = models.CharField(null=True,blank=True,max_length=20,verbose_name="電話號碼")
